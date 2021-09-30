8824cb386f294a8fb3d30939f9d064aab9268ef5
Daniel <danielood@hotmail.es>
2021-10-01 00:28:33 +0200

    Subida inicial proyecto

---
+++ src/main/java/com/proyectogrado/proyectorobotserver/launcher/Launcher.java
@@ -0,0 +1,9 @@
+package com.proyectogrado.proyectorobotserver.launcher;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/com/proyectogrado/proyectorobotserver/util/ConexionUtil.java
@@ -0,0 +1,4 @@
+package com.proyectogrado.proyectorobotserver.util;
+
+public class ConexionUtil {
+}
